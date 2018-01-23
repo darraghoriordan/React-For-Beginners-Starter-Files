cf8a0e9073feb4c90cda56eb04803afea5455e74
Darragh ORiordan <darragh.oriordan@gmail.com>
2018-01-23 13:14:26 +1300

    up to 6

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Inventory extends React.Component{
+
+    render(){
+        return (
+            <p>inventory</p>
+        )
+    }
+}
+
+export default Inventory
